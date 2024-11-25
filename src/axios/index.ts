38808d2a8dccee4238903ab410ac5352dffae8c8
thakiyudheen <thakeyudheen525@gmail.com>
2024-11-25 08:29:15 +0530

    setup

--- src/axios/index.ts
+++ src/axios/index.ts
@@ -3,7 +3,9 @@ import { logoutAction } from '@/store/actions/auth/logoutAction';
 import axios, { AxiosError, AxiosResponse } from 'axios';
 
 
+// const BASE_URL: string = process.env.NEXT_PUBLIC_BASE_URL ||"";
 const BASE_URL: string = process.env.NEXT_BASE_URL||'http://localhost:3001';
+console.log(process.env.NEXT_BASE_URL);
 
 export const api_client = axios.create({
   baseURL: BASE_URL, 
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,7 +9,7 @@ import { GoogleOAuthProvider } from '@react-oauth/google';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <GoogleOAuthProvider clientId='772859590844-n5b3qkh0hjcmje3r8sg82io0u1v214sc.apps.googleusercontent.com'>
+    <GoogleOAuthProvider clientId={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID||""}>
       <Provider store={store}>
         <AuthProvider>
           <Component {...pageProps} />
